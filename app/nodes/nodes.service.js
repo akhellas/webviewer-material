e9be50ca4db6240243d49ec65b3f004e8feb190e
akhellas <ak.hellas@gmail.com>
2016-06-29 10:50:08 +0300

    nodes mock

--- app/nodes/nodes.service.js
+++ app/nodes/nodes.service.js
@@ -9,28 +9,33 @@ class nodesService {
                 Id: '1',
                 Title: 'Category 1',
                 Color: '',
+                InOverview: false,
                 Children: [
                     {
                         Id: '11',
                         Title: 'Sub Category 11',
                         Color: '',
+                        InOverview: false,
                         Children: [
                             {
                                 Id: '111',
                                 Title: 'Sub Item 111',
                                 Color: '',
+                                InOverview: false,
                                 Children: []
                             },
                             {
                                 Id: '112',
                                 Title: 'Sub Item 112',
                                 Color: '',
+                                InOverview: false,
                                 Children: []
                             },
                             {
                                 Id: '113',
                                 Title: 'Sub Item 113',
                                 Color: '',
+                                InOverview: false,
                                 Children: []
                             }
                         ]
@@ -39,11 +44,18 @@ class nodesService {
                         Id: '12',
                         Title: 'Sub Category 12',
                         Color: '',
+                        InOverview: false,
                         Children: []
                     }
                 ] 
             },
-            { Id: '2', Title: 'Category 2', Color: '', Children: [] }
+            {
+                Id: '2',
+                Title: 'Category 2',
+                Color: '',
+                InOverview: false,
+                Children: []
+            }
         ]
     }
 }
