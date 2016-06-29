68f73aac42ed110116e3234d836df802c651eb37
Nikpds <perpegr@hotmail.com>
2016-06-29 15:01:39 +0300

    admin

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -3,6 +3,19 @@ class adminController {
 
         this.nodes = nodesService.getNodes();
         this.overviewShow = false;
+
+        this.openSubCategory = function (index) {
+            this.selected = index;
+            this.subSelected = null;
+        };
+
+        this.subSelected;
+        this.swap = function (nodes, index1, index2) {
+
+            var tmp = nodes[index1];
+            nodes[index1] = nodes[index2];
+            nodes[index2] = tmp;
+        }
     }
 }
 
