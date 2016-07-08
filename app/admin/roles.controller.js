aa946f317e3fdfae4e144c0ff34efa8d8b0d0180
Nikpds <perpegr@hotmail.com>
2016-07-08 08:51:27 +0300

    option

--- app/admin/roles.controller.js
+++ app/admin/roles.controller.js
@@ -1,52 +1,80 @@
 class rolesController {
     constructor(authService, nodesService) {
 
-        this.nodeRights = authService.roles;
+        this.nodeRights = [
+            {
+                Id: '1',
+                Title: 'ΚΕΠΙΧ',
+                Description: 'bla',
+                Rights: [{ _Id: '21', Title: 'ΔΡΑΣΤΗΡΙΟΤΗΤΑ', Edit: true, View: false }]
+            },
+            {
+                Id: '2',
+                Title: 'ΕΠΙΧΕΙΡΗΣΕΙΣ',
+                Description: 'bla',
+                Rights: [{}]
+            },
+            {
+                Id: '3',
+                Title: 'ΗΓΕΣΙΑ',
+                Description: 'bla',
+                Rights: [{}]
+            },
+            {
+                Id: '4',
+                Title: 'ΔΙΑΧΕΙΡΙΣΤΕΣ',
+                Description: 'bla',
+                Rights: [{}]
+            }
+        ];
         this.nodes = nodesService.nodes;
 
         this.editOn = false;
         this.nodeSelected;
+
+    }
+    addGroup() {
+        this.nodeRights.push({
+            Id: '', Title: 'Νέα ομάδα', Description: 'Περιγραφή', Rights: []
+        });
+        this.nodeSelected = this.nodeRights.length - 1
+    }
+
+    save() {
+        this.editOn = false;
+    }
+
+    cancelAddCategory() {
+        this.addright = !this.addright;
         this.newRight = {
             Edit: false,
             View: false
         };
-
-
-        this.addGroup = function () {
-            this.nodeRights.push({
-                Id: '', Title: 'Νέα ομάδα', Description: 'Περιγραφή', Rights: []
-            });
-            this.nodeSelected = this.nodeRights.length - 1
-        }
-
-        this.pushRight = function () {
-            this.nodeRights[this.nodeSelected].Rights.push({
-                _Id: '',
-                Title: this.newRight.Title,
-                Edit: this.newRight.Edit,
-                View: this.newRight.View
-            });
-            this.addright = !this.addright;
-            this.newRight = {
-                Edit: false, View: false
-            };
-        }
-        this.removeRight = function () {
-            this.nodeRights[this.nodeSelected].Rights
-        }
-
-        this.cancelAddCategory = function () {
-            this.addright = !this.addright;
-            this.newRight = {
-                Edit: false,
-                View: false
-            };
-        }
-        this.save = function () {
-            this.editOn = false;
-        }
     }
-    
+
+    removeRight() {
+        this.nodeRights[this.nodeSelected].Rights
+    }
+
+    pushRight() {
+        this.newRight = {
+            Edit: false, View: false
+        };
+
+        this.nodeRights[this.nodeSelected].Rights.push({
+            _Id: '',
+            Title: this.newRight.Title,
+            Edit: this.newRight.Edit,
+            View: this.newRight.View
+        });
+        this.addright = !this.addright;
+        
+        this.newRight = {
+            Edit: false, View: false
+        };
+    }
+
+
 }
 
 export default rolesController;
