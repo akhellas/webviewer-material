c4b802145fcd2820992cda363c312c22daf27de0
Nikpds <perpegr@hotmail.com>
2016-07-15 12:21:36 +0300

    form.html changes not finished

--- app/nodes/form.controller.js
+++ app/nodes/form.controller.js
@@ -2,16 +2,6 @@ class formController {
 
     constructor() {
 
-        this.PlaneTypes = [
-            { Id: 0, Description: 'F-16' },
-            { Id: 1, Description: 'F-4' },
-            { Id: 2, Description: 'CN-235' },
-            { Id: 3, Description: 'KC-135' },
-            { Id: 4, Description: 'AB-412' },
-            { Id: 5, Description: 'AB-212' },
-            { Id: 6, Description: 'S-70' },
-            { Id: 7, Description: 'Ε/Π' }
-        ];
     }
 }
 
--- app/nodes/node.controller.js
+++ app/nodes/node.controller.js
@@ -2,14 +2,60 @@ class nodeController {
 
     constructor(node) {
         this.node = node;
+        this.PlaneTypes = [
+            { Id: 0, Description: 'F-16' },
+            { Id: 1, Description: 'F-4' },
+            { Id: 2, Description: 'CN-235' },
+            { Id: 3, Description: 'KC-135' },
+            { Id: 4, Description: 'AB-412' },
+            { Id: 5, Description: 'AB-212' },
+            { Id: 6, Description: 'S-70' },
+            { Id: 7, Description: 'Ε/Π' }
+        ];
+        this.newEntry = {
+            Violations: [{}],
+            Units: [{}],
+            FlyOvers: [{}]
+        };
+    }
+    // form code
+    addUnit() {
+        this.newEntry.Units.push({
+        });
     }
 
+     addViolations() {
+        this.newEntry.Violations.push({
+        });
+    }
+    
+     addFlyOvers() {
+        this.newEntry.FlyOvers.push({
+        });
+    }
+
+    removeUnit(index) {
+        this.newEntry.Units.splice(index, 1);
+    }
+
+     removeFlyOvers(index) {
+        this.newEntry.FlyOvers.splice(index, 1);
+    }
+
+     removeViolations(index) {
+        this.newEntry.Violations.splice(index, 1);
+    }
+
+    pushNewEntry() {
+
+    }
+    // end of form code
     sumFormations() {
-        return this.sumIncidents( (f) => { return f.Units.length; });
+        return this.sumIncidents((f) => { return f.Units.length; });
     }
 
     sumAcType(acType) {
-        return this.sumIncidents( (f) => { let t = f.Units.find(x => x.Type == acType); return t == null ? 0 : t.Count; });
+        return this.sumIncidents((f) => { let t = f.Units.find(x => x.Type == acType); return t == null ? 0 : t.Count; });
     }
 
     sumIncidents(cbField) {
