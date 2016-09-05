10711d1b3a51cb10a9d26dc5420531266dce037e
Nikpds <perpegr@hotmail.com>
2016-09-05 12:08:10 +0300

    changes in view html,js

--- app/admin/view.controller.js
+++ app/admin/view.controller.js
@@ -1,7 +1,59 @@
 class viewController {
-    constructor(adminService) {
-        this.views = adminService.get;
+    constructor($scope, nodesService) {
+        this.$scope = $scope;
+        this.nodesService = nodesService;
+        this.views = [
+            {
+                Id: '21321',
+                Description: 'ΤΟΥΡΚΙΚΗ ΚΑΤΑΣΤΑΣΗ',
+                nodeViews: [
+                    {
+                        Id: '1',
+                        Description: 'ΔΡΑΣΤΗΡΙΟΤΗΤΑ'
+                    },
+                    {
+                        Id: '21',
+                        Description: 'ΒΟΛΕΣ ΠΝ & ΣΞ'
+                    },
+                    {
+                        Id: '12',
+                        Description: 'ΤΟΥΡΚΙΚΗ ΠΤΗΤΙΚΗ ΔΡΑΣΤΗΡΙΟΤΗΤΑ'
+                    }
+                ]
+            },
+            {
+                Id: '542',
+                Description: 'ΠΡΟΒΟΛΗ RADAR',
+                nodeViews: [
+                    {
+                        Id: '1',
+                        Description: 'ΠΤΗΤΙΚΗ ΔΡΑΣΤΗΡΙΟΤΗΤΑ'
+                    },
+                    {
+                        Id: '21',
+                        Description: 'ΑΤΑ'
+                    },
+                    {
+                        Id: '12',
+                        Description: 'ΕΠΧΣΕΙΣ-ΑΣΚΗΣΕΙΣ'
+                    }
+                ]
+            }
+
+        ];
+
+
+        this.$scope.$watch('view.nodesService.nodes', (x) => this.nodesChanged(x), true);
     }
+    nodesChanged(newValue, oldValue) {
+        if (newValue != undefined && newValue != oldValue) {
+            this.nodes = newValue;
+        }
+    }
+
+
+
+
 
 }
 export default viewController;
